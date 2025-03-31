8efb83da367b91df628294d4fc851ca6c680dcb0
Lukas Piwowarski <lpiwowar@redhat.com>
2025-03-31 15:56:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# RCAccelerator Tools
+
+This repository contains useful tools that can be used to deploy the
+[RCAccelerator/chatbot](https://github.com/RCAccelerator/chatbot). As of now,
+this repository contains:
+
+- `jira_scraper.py`: a script that scrapes Jira and stores the collected data
+in a vector database.
---
+++ jira_scraper.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import uuid
+from argparse import ArgumentParser
+import json
+import multiprocessing as mp
+
+import requests
+import pandas as pd
+from tqdm import tqdm
+
+from langchain.text_splitter import (
+    RecursiveCharacterTextSplitter,
+)
+from transformers import AutoTokenizer
+from openai import OpenAI
+from qdrant_client import QdrantClient, models
+
+
+COLLECTION_NAME = "all-jira-tickets"
+TOKENIZER_MODEL = "BAAI/bge-large-en-v1.5"
+
+DEFAULT_EMBEDDING_MODEL_ID = "BAAI/bge-large-en-v1.5"
+
+DEFAULT_JIRA_PROJECTS = [
+    "OSP",
+    "RHOSINFRA",
+    "OSPCIX",
+    "RHOSBUGS",
+    "OSPK8",
+    "RHOSPPRIO",
+    "OSPRH",
+]
+
+
+def get_jira_data(
+        jira_url: str, headers: dict, query: str, max_results: int, start_at: int):
+    """Retrieve data from JIRA"""
+    full_url = f"{jira_url}/rest/api/2/search?jql={query}&maxResults={max_results}&fields=*all&startAt={start_at}"
+    print(f"Getting: {full_url}")
+    try:
+        response = requests.get(
+            full_url,
+            headers=headers,
+            timeout=(3.05, 180),
+        )
+
+    except requests.exceptions.Timeout:
+        print(f"Request to {jira_url} failed with time out.")
+        return []
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+    else:
+        print(f"Couldn't retrieve data from JIRA {response.status_code} for {full_url}")
+        return []
+
+    return data["issues"]
+
+
+def update_database(
+    database_client_url: str,
+    llm_server_url: str,
+    llm_api_key: str,
+    jira_token: str,
+    embedding_model: str,
+    database_api_key: str,
+    max_results: int = 10000,
+    jira_url: str = "https://issues.redhat.com",
+    chunk_size: int = 1024,
+    jira_database_bkp_path: str = "jira_all_bugs.pickle",
+    jira_projects: list = DEFAULT_JIRA_PROJECTS,
+):
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {jira_token}",
+    }
+    results = []
+
+    projects = "&OR&".join([f"project={e}" for e in jira_projects])
+    query = f"{projects}&AND&type=bug&AND&status=Closed"
+    # Get initial batch of data
+    try:
+        response = requests.get(
+            f"{jira_url}/rest/api/2/search?jql={query}&maxResults={max_results}&fields=*all",
+            headers=headers,
+            timeout=(3.05, 180),
+            )
+    except requests.exceptions.Timeout as e:
+        print(f"Request to {jira_url} failed with time out.")
+        raise e
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+    else:
+        print(f"Couldn't retrieve any data from JIRA {response.status_code}")
+        raise ValueError
+
+    total = data["total"]
+    print(f"{total} items found for query {query}")
+
+    # Retrieve results going further back
+    with mp.Pool(10) as pool:
+        results = pool.starmap(
+            get_jira_data,
+            [(jira_url, headers, query, max_results, page) for page in range(1000, total, 1000)])
+
+    results.append(data["issues"])
+
+    # Flatten the list of issues
+    results = [issue for e in results for issue in e]
+
+    dataset = []
+
+    for raw_result in tqdm(results):
+        row = {}
+        row["id"] = raw_result["id"]
+        row["url"] = f"{jira_url}/browse/{raw_result['key']}"
+        row["summary"] = raw_result["fields"]["summary"]
+        row["description"] = raw_result["fields"]["description"]
+        row["comments"] = "\n".join(
+            [comment["body"] for comment in raw_result["fields"]["comment"]["comments"]])
+        dataset.append(row)
+
+    df = pd.DataFrame(dataset)
+    df["text"] = df.summary + "\n" + df.description.fillna(" ") + df.comments
+
+    # Remove duplicates
+    df = df.drop_duplicates(subset=["id"])
+
+    print(df.info())
+
+    llm = OpenAI(
+        base_url=llm_server_url,
+        organization="",
+        api_key=llm_api_key,
+    )
+
+    # Back up data
+    df.to_pickle(jira_database_bkp_path)
+
+    ## Upload data
+    client = QdrantClient(database_client_url, api_key=database_api_key)
+
+    tokenizer = AutoTokenizer.from_pretrained(embedding_model)
+
+    splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+        tokenizer, chunk_size=chunk_size, chunk_overlap=0
+    )
+
+    client.recreate_collection(
+        collection_name=COLLECTION_NAME,
+        vectors_config=models.VectorParams(size=1024, distance=models.Distance.COSINE),
+    )
+
+    for _, row in tqdm(df.iterrows()):
+        chunks = splitter.split_text(row["text"])
+        for _, chunk in enumerate(chunks):
+            embedding = (
+                llm.embeddings.create(model=embedding_model, input=chunk)
+                .data[0]
+                .embedding
+            )
+            client.upsert(
+                collection_name=COLLECTION_NAME,
+                points=[
+                    models.PointStruct(
+                        id=str(uuid.uuid4()),
+                        payload={"url": row["url"], "text": row["text"]},
+                        vector=embedding,
+                    ),
+                ],
+            )
+
+    print(
+        f"Number of records: {client.get_collection(collection_name=COLLECTION_NAME).points_count}"
+    )
+
+
+def main():
+    parser = ArgumentParser("jira_scraper")
+    parser.add_argument("--jira_url", type=str, default="https://issues.redhat.com")
+    parser.add_argument("--jira_token", type=str)
+    parser.add_argument("--max_results", type=int, default=10000)
+    parser.add_argument("--chunk_size", type=int, default=1024)
+    parser.add_argument("--database_client_url", type=str, default="")
+    parser.add_argument("--database_api_key", type=str, default="")
+    parser.add_argument("--llm_server_url", type=str, default="")
+    parser.add_argument("--llm_api_key", type=str, default="")
+    parser.add_argument("--embedding_model", type=str, default=DEFAULT_EMBEDDING_MODEL_ID)
+    parser.add_argument("--jira_projects", nargs='+', type=str, default=DEFAULT_JIRA_PROJECTS)
+    args = parser.parse_args()
+
+    update_database(
+        llm_server_url=args.llm_server_url,
+        llm_api_key=args.llm_api_key,
+        jira_token=args.jira_token,
+        embedding_model=args.embedding_model,
+        max_results=args.max_results,
+        jira_url=args.jira_url,
+        chunk_size=args.chunk_size,
+        database_client_url=args.database_client_url,
+        jira_projects=args.jira_projects,
+        database_api_key=args.database_api_key,
+    )
+
+
+if __name__ == "__main__":
+    main()
