301cd8bcb2133fd6df7f86a118fd919dd373294d
rabi <ramishra@redhat.com>
2025-04-04 15:10:24 +0530

    Refactor jira_scraper tool
    This refactors the code to split into modules for better
    maintainability and also adds a few improvements.
    
    - Don't assume embedding dimension to be fixed
    - Add required fields to ArgumentParser
    
    Signed-off-by: rabi <ramishra@redhat.com>

---
+++ jira_scraper/common/constants.py
@@ -0,0 +1,11 @@
+"""Module for config constants."""
+
+COLLECTION_NAME = "all-jira-tickets"
+DEFAULT_EMBEDDING_MODEL = "BAAI/bge-large-en-v1.5"
+DEFAULT_JIRA_URL = "https://issues.redhat.com"
+DEFAULT_JIRA_PROJECTS = [
+    "OSP", "RHOSINFRA", "OSPCIX", "RHOSBUGS",
+    "OSPK8", "RHOSPRIO", "OSPRH"
+]
+DEFAULT_CHUNK_SIZE = 1024
+DEFAULT_MAX_RESULTS = 10000
---
+++ jira_scraper/main.py
@@ -0,0 +1,51 @@
+"""Main module for jira scraper."""
+
+from argparse import ArgumentParser
+from jira_scraper.common import constants
+from jira_scraper.core.scraper import JiraScraper
+
+
+def command():
+    """Entry point for command line execution."""
+    parser = ArgumentParser("jira_scraper")
+
+    # Required arguments
+    parser.add_argument("--jira_token", type=str, required=True)
+    parser.add_argument("--database_client_url", type=str, required=True)
+    parser.add_argument("--llm_server_url", type=str, required=True)
+    parser.add_argument("--llm_api_key", type=str, required=True)
+    parser.add_argument("--database_api_key", type=str, required=True)
+
+    # Optional arguments
+    parser.add_argument("--jira_url", type=str,
+                        default=constants.DEFAULT_JIRA_URL)
+    parser.add_argument("--max_results", type=int,
+                        default=constants.DEFAULT_MAX_RESULTS)
+    parser.add_argument("--chunk_size", type=int,
+                        default=constants.DEFAULT_CHUNK_SIZE)
+    parser.add_argument("--embedding_model", type=str,
+                        default=constants.DEFAULT_EMBEDDING_MODEL)
+    parser.add_argument("--jira_projects", nargs='+', type=str,
+                        default=constants.DEFAULT_JIRA_PROJECTS)
+
+    args = parser.parse_args()
+
+    config_args = {
+        "jira_token": args.jira_token,
+        "database_client_url": args.database_client_url,
+        "llm_server_url": args.llm_server_url,
+        "llm_api_key": args.llm_api_key,
+        "database_api_key": args.database_api_key,
+        "jira_url": args.jira_url,
+        "max_results": args.max_results,
+        "chunk_size": args.chunk_size,
+        "embedding_model": args.embedding_model,
+        "jira_projects": args.jira_projects,
+    }
+
+    scraper = JiraScraper(config_args)
+    scraper.run()
+
+
+if __name__ == "__main__":
+    command()
