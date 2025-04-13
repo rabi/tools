e18aa10a5f6936bfc4abc0468e4a0084568a64a4
Lukas Piwowarski <lpiwowar@redhat.com>
2025-04-13 15:39:59 +0200

    Remove key field
    The key is already stored in the URL. There is no need to store it
    twice.

--- jira_scraper/core/scraper.py
+++ jira_scraper/core/scraper.py
@@ -25,8 +25,6 @@ class JiraRecord(TypedDict, total=False):
         text: The content extracted from a specific field (e.g., summary,
             description, comment).
         id: A unique identifier assigned to the ticket (e.g., 16238715).
-        key: The ticket's key, combining the project name and ticket number
-            (e.g., OSPRH-1234).
         url: The URL pointing directly to the Jira ticket
             (e.g., http://issues.redhat.com/OSPRH-1234).
         kind: Specifies the type of field the data originates from (e.g.,
@@ -137,7 +135,6 @@ class JiraScraper:
                     "kind": text_field_name,
                     "fix_versions": fix_versions,
                     "url": jira_url,
-                    "key": issue["key"],
                 })
 
             # Concatenate all comments for a jira
@@ -153,7 +150,6 @@ class JiraScraper:
                 "kind": "comment",
                 "fix_versions": fix_versions,
                 "url": jira_url,
-                "key": issue["key"],
             })
 
         return jira_records
